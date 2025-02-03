bc1caaa44f43d04658a5592590225e72781242c9
saurabh-develop <saurabhkhare893@gmail.com>
2025-02-04 01:48:12 +0530

    Drag and drop feature made

--- client/src/components/Chess/ChessBoard.jsx
+++ client/src/components/Chess/ChessBoard.jsx
@@ -3,7 +3,43 @@ import { MOVE } from "./ChessGame";
 
 const ChessBoard = ({ chess, board, socket, setBoard }) => {
   const [from, setFrom] = useState(null);
-  const [to, setTo] = useState(null);
+  const handleDragStart = (e, squareRepresentation) => {
+    setFrom(squareRepresentation);
+    e.dataTransfer.setData("text/plain", squareRepresentation);
+  };
+  const handleDrop = (e, toSquareRepresentation) => {
+    e.preventDefault();
+    const fromSquareRepresentation = e.dataTransfer.getData("text/plain");
+
+    if (fromSquareRepresentation !== toSquareRepresentation) {
+      socket.send(
+        JSON.stringify({
+          type: MOVE,
+          payload: {
+            move: {
+              from: fromSquareRepresentation,
+              to: toSquareRepresentation,
+            },
+          },
+        })
+      );
+
+      chess.move({
+        from: fromSquareRepresentation,
+        to: toSquareRepresentation,
+      });
+      setBoard(chess.board());
+      console.log({
+        from: fromSquareRepresentation,
+        to: toSquareRepresentation,
+      });
+    }
+    setFrom(null);
+  };
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+  };
   return (
     <div className="text-white">
       {board.map((row, i) => (
@@ -40,12 +76,20 @@ const ChessBoard = ({ chess, board, socket, setBoard }) => {
                     });
                   }
                 }}
+                onDrop={(e) => handleDrop(e, squareRepresentation)}
+                onDragOver={handleDragOver}
                 key={j}
                 className={`w-20 h-20 flex items-center justify-center text-black
-                ${(i + j) % 2 == 0 ? "bg-green-600" : "bg-green-100"}`}
+                ${(i + j) % 2 != 0 ? "bg-green-600" : "bg-green-100"}`}
               >
                 <div className="w-full justify-center flex h-full">
-                  <div className="h-full justify-center flex flex-col">
+                  <div
+                    className="h-full justify-center flex flex-col"
+                    draggable={!!square}
+                    onDragStart={(e) =>
+                      handleDragStart(e, squareRepresentation)
+                    }
+                  >
                     {square ? (
                       <img
                         className="w-20"
